1bb7afc1e073f072410d4ab2a597b10216e402ba
moi-taiga <moi.t.nicholas@gmail.com>
2024-01-26 11:32:13 +0000

    Resolved discussed comments.

--- omics/omics_saver.py
+++ omics/omics_saver.py
@@ -1,14 +1,11 @@
 import os
-import sys
-# import random
-
 import numpy as np
 import pandas as pd
 
 
 # function to split counts matrix into a set of 1D np arrays, append class label and save as individual files
 
-def matrixsplitsave(omic_mat, class_vector, output_path):
+def matrixsplitsave(omic_mat, class_vector, path_out):
     sample_names_vec = omic_mat.columns
 
     gene_array_lst = list(range(omic_mat.shape[1]))
@@ -18,20 +15,19 @@ def matrixsplitsave(omic_mat, class_vector, output_path):
 
     # Make sure that the user has a directory named input_arrays.
     # if not, create it.
-    input_arrays_path = os.path.join(output_path, 'input_arrays')
+    input_arrays_path = os.path.join(path_out, 'input_arrays')
     if not os.path.exists(input_arrays_path):
         os.makedirs(input_arrays_path)
         print(f"Directory '{input_arrays_path}' created.")
 
-
-    # save each array with a file name prefixed with its class label
+# save each array with a file name prefixed with its class label
 
     for sid, sample, label in zip(sample_names_vec, gene_array_lst, class_vector):
         sample_name = str(label) + '_' + str(sid) + '.npy'
         sample = np.array(sample)
 
         # Build save path
-        save_path = os.path.join(output_path, 'input_arrays', sample_name)
+        save_path = os.path.join(path_out, 'input_arrays', sample_name)
 
         # save the sample arrays
         np.save(save_path, sample)
@@ -45,7 +41,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    # -db DATABSE -u USERNAME -p PASSWORD -size 20
+    # -db DATABASE -u USERNAME -p PASSWORD -size 20 # MTN is this needed?
     parser.add_argument("--matrix_file", help="Path to rna-seq counts matrix csv file")
     parser.add_argument("--labels_file", help="Path to class labels csv file")
     parser.add_argument("--output_path", help="Output path to save data")
@@ -58,16 +54,16 @@ if __name__ == '__main__':
 
     # read in counts matrix and classes
 
-    data_matrix = pd.read_csv(matrix_path, header=0) # MTN should we use omic.mat instead of data_matrix? omic_mat is not defined?
+    data_matrix = pd.read_csv(matrix_path, header=0)
     sample_classes = pd.read_csv(labels_path, header=0).values.flatten()
-    sample_classes_unique = np.unique(sample_classes).reshape(1,2) # MTN why do we need to reshape this? lets use -1 instead of 2 in order to handle any number of classes
-    #
+    sample_classes_unique = np.unique(sample_classes).reshape(1, -1)  # -1 allows for variable number of classes
+
     # save the unique list of classes as a csv file
 
-    np.savetxt(output_path + 'class_lst.csv', sample_classes_unique, fmt='%i',  delimiter=",") #MTN lets use os.path.join(output_path, 'class_lst.csv')
+    np.savetxt(output_path + 'class_lst.csv', sample_classes_unique, fmt='%i',  delimiter=",")  # MTN lets use os.path)
 
     # split matrix and save as individual files
-    matrixsplitsave(data_matrix, sample_classes, output_path) # MTN should we use omic.mat instead of data_matrix? omic_mat is not defined?
+    matrixsplitsave(omic_mat=data_matrix, class_vector=sample_classes, path_out=output_path)
 
 # next steps to add
 # randomised train test split
